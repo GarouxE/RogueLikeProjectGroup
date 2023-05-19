f875033609057ea97fa9bfa630324c1949eb8174
GarouxE <133769796+GarouxE@users.noreply.github.com>
2023-05-19 14:24:20 +0200

    Update Game.py

--- Game.py
+++ Game.py
@@ -5,36 +5,38 @@ from Hero import Hero
 from Map import Map
 from Stairs import Stairs
 from handler import heal, teleport, throw
-from utils import _find_getch
+
+from utils import getch
 import theGame
 
 import random, copy
+
 
 class Game(object):
     """ Class representing game state """
 
     """ available equipments """
-    equipments = {0: [Equipment("potion", "!", usage=lambda self, hero: heal(hero)), \
-                      Equipment("gold", "o")], \
-                  1: [Equipment("potion", "!", usage=lambda self, hero: teleport(hero, True))], \
-                  2: [Equipment("bow", usage=lambda self, hero: throw(1, True))], \
-                  3: [Equipment("portoloin", "w", usage=lambda self, hero: teleport(hero, False))], \
+    equipments = {0: [Equipment("potion", "!", usage=lambda self, hero: heal(hero)),
+                      Equipment("gold", "o")],
+                  1: [Equipment("potion", "!", usage=lambda self, hero: teleport(hero, True))],
+                  2: [Equipment("bow", usage=lambda self, hero: throw(1, True))],
+                  3: [Equipment("portoloin", "w", usage=lambda self, hero: teleport(hero, False))],
                   }
     """ available monsters """
     monsters = {0: [Creature("Goblin", 4), Creature("Bat", 2, "W")],
                 1: [Creature("Ork", 6, strength=2), Creature("Blob", 10)], 5: [Creature("Dragon", 20, strength=3)]}
 
     """ available actions """
-    _actions = {'z': lambda h: theGame.theGame()._floor.move(h, Coord(0, -1)), \
-                'q': lambda h: theGame.theGame()._floor.move(h, Coord(-1, 0)), \
-                's': lambda h: theGame.theGame()._floor.move(h, Coord(0, 1)), \
-                'd': lambda h: theGame.theGame()._floor.move(h, Coord(1, 0)), \
-                'i': lambda h: theGame.theGame().addMessage(h.fullDescription()), \
-                'k': lambda h: h.__setattr__('hp', 0), \
-                'u': lambda h: h.use(theGame.theGame().select(h._inventory)), \
-                ' ': lambda h: None, \
-                'h': lambda hero: theGame.theGame().addMessage("Actions disponibles : " + str(list(Game._actions.keys()))), \
-                'b': lambda hero: theGame.theGame().addMessage("I am " + hero.name), \
+    _actions = {'z': lambda h: theGame.theGame()._floor.move(h, Coord(0, -1)),
+                'q': lambda h: theGame.theGame()._floor.move(h, Coord(-1, 0)),
+                's': lambda h: theGame.theGame()._floor.move(h, Coord(0, 1)),
+                'd': lambda h: theGame.theGame()._floor.move(h, Coord(1, 0)),
+                'i': lambda h: theGame.theGame().addMessage(h.fullDescription()),
+                'k': lambda h: h.__setattr__('hp', 0),
+                'u': lambda h: h.use(theGame.theGame().select(h._inventory)),
+                ' ': lambda h: None,
+                'h': lambda hero: theGame.theGame().addMessage("Actions disponibles : " + str(list(Game._actions.keys()))),
+                'b': lambda hero: theGame.theGame().addMessage("I am " + hero.name),
                 }
 
     def __init__(self, level=1, hero=None):
@@ -81,7 +83,7 @@ class Game(object):
 
     def select(self, l):
         print("Choose item> " + str([str(l.index(e)) + ": " + e.name for e in l]))
-        c = _find_getch()
+        c = getch()
         if c.isdigit() and int(c) in range(len(l)):
             return l[int(c)]
 
@@ -94,7 +96,7 @@ class Game(object):
             print(self._floor)
             print(self._hero.description())
             print(self.readMessages())
-            c = _find_getch()
+            c = getch()
             if c in Game._actions:
                 Game._actions[c](self._hero)
             self._floor.moveAllMonsters()
